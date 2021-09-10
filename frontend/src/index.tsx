e02f30e6d2cfcfa0e8ea9b42f16fda29f95a97bb
wellinton-dev <wellinton.dev@gmail.com>
2021-09-10 12:35:00 -0300

    Routes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/css/styles.css";
 import App from "./App";
+import NavBar from 'components/NavBar/index'
+import Footer from "components/Footer/index";
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavBar />
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById("root")
 );
