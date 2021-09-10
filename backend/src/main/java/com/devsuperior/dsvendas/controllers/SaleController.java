afec93f91d07c689306019b7b490331a5ff22227
wellinton-dev <wellinton.dev@gmail.com>
2021-09-10 19:37:45 -0300

    BarChart integration

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -34,7 +34,7 @@ public class SaleController {
 		return ResponseEntity.ok(list);
 	}
 	
-	@GetMapping(value= "/sucess-by-seller")
+	@GetMapping(value= "/success-by-seller")
 	public ResponseEntity<List<SaleSucessDTO>> succesGroupedBySeller(){
 		List<SaleSucessDTO> list = saleService.sucessGroupedBySeller();
 		return ResponseEntity.ok(list);
--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,40 +1,72 @@
-import Chart from 'react-apexcharts'
+import axios from "axios";
+import { round } from "pages/utils/format";
+import { BASE_URL } from "pages/utils/requests";
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import { SaleSuccess } from "types/sale";
+
+type SeriesData =  {
+  name: string;
+  data: number[];
+};
+type ChartData =  {
+  labels: {
+    categories: string[];
+  };
+  series: SeriesData[];
+};
 
 const BartChart = () => {
-    const options = {
-      plotOptions: {
-        bar: {
-          horizontal: true,
-        },
-      },
-    };
+  const [chartData, setChartData] = useState<ChartData>({
 
-    const mockData = {
-      labels: {
-        categories: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
+    labels: {
+      categories: []
+    },
+
+    series: [
+      {
+        name: "",
+        data: [],
       },
-      series: [
-        {
-          name: "% Sucesso",
-          data: [43.6, 67.1, 67.7, 45.6, 71.1],
+    ],
+  });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then((response) => {
+      const data = response.data as SaleSuccess[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => round((100 * x.deals) / x.visited, 1));
+
+      setChartData({
+        labels: {
+          categories: myLabels,
         },
-      ],
-    };
+        series: [
+          {
+            name: "% Sucesso",
+            data: mySeries,
+          },
+        ],
+      });
+    });
+  }, [0]);
+
+  const options = {
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
+  };
+
   return (
-
     <Chart
-        options={{...options, xaxis: mockData.labels}}
-        series ={mockData.series}
-        type="bar"
-        height= "240px"
+      options={{ ...options, xaxis: chartData.labels }}
+      series={chartData.series}
+      type="bar"
+      height="240px"
     />
-
-    
-    
-    );
+  );
 };
 
 export default BartChart;
-
-
-
